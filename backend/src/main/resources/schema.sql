2fa92f9de1f891b311a86055ec0656c0e15a9887
anya <804anya@gmail.com>
2025-10-14 13:59:50 +0000

    new data.sql file for adding example customers

--- backend/src/main/resources/schema.sql
+++ backend/src/main/resources/schema.sql
@@ -5,7 +5,5 @@ CREATE TABLE customers (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP NOT NULL,
-    updated_at TIMESTAMP NOT NULL
+    password VARCHAR(255) NOT NULL
 );
