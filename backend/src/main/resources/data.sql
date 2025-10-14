b7c6fca405f3e0fab248f355cd4d47d189f17478
anya <804anya@gmail.com>
2025-10-14 14:24:24 +0000

    .sql ammendments and configuring application properties

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,4 +1,4 @@
-insert into CUSTOMERS (NAME, EMAIL, PASSWORD) VALUES ('admin', 'admin@admin.com', 'admin');
-insert into CUSTOMERS (NAME, EMAIL, PASSWORD) VALUES ('Bruce', 'bruce@a.com', 'password1');
-insert into CUSTOMERS (NAME, EMAIL, PASSWORD) VALUES ('Paul', 'paul@b.com', 'password2');
-insert into CUSTOMERS (NAME, EMAIL, PASSWORD) VALUES ('Rick', 'rick@c.com', 'password3');
+insert into CUSTOMERS (CUSTOMER_NAME, EMAIL, PASSWORD) VALUES ('admin', 'admin@admin.com', 'admin');
+insert into CUSTOMERS (CUSTOMER_NAME, EMAIL, PASSWORD) VALUES ('Bruce', 'bruce@a.com', 'password1');
+insert into CUSTOMERS (CUSTOMER_NAME, EMAIL, PASSWORD) VALUES ('Paul', 'paul@b.com', 'password2');
+insert into CUSTOMERS (CUSTOMER_NAME, EMAIL, PASSWORD) VALUES ('Rick', 'rick@c.com', 'password3');
