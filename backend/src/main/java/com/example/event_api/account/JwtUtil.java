04b8100072a357e92e9c8d3b2ac3cc8adce504c0
gcoearcher <gcoearchermaillo@gmail.com>
2025-10-16 10:17:49 +0000

    update JwtUtil to enable the jwt methods

--- backend/src/main/java/com/example/event_api/account/JwtUtil.java
+++ backend/src/main/java/com/example/event_api/account/JwtUtil.java
@@ -1,9 +1,10 @@
 package com.example.event_api.account;
 
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.security.Keys;
+//import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Component;
 import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
 import java.nio.charset.StandardCharsets;
 import java.util.Date;
 import io.jsonwebtoken.SignatureAlgorithm;
@@ -12,28 +13,30 @@ import io.jsonwebtoken.SignatureAlgorithm;
 public class JwtUtil {
     //use secret key so both Account and Data services can share it
     private static final String SECRET_KEY_STRING = "MySecretKeyForJWTTokenValidationMustBeLongEnoughForHS256Algorithm";
-    private final SecretKey jwtSecret = Keys.hmacShaKeyFor(SECRET_KEY_STRING.getBytes(StandardCharsets.UTF_8));
-
+    //private final SecretKey jwtSecret = Keys.hmacShaKeyFor(SECRET_KEY_STRING.getBytes(StandardCharsets.UTF_8));
+    private final SecretKey jwtSecret = new SecretKeySpec(SECRET_KEY_STRING.getBytes(StandardCharsets.UTF_8), "HmacSHA256");
     public String generateToken(String username) {
         return Jwts.builder()
                 .setSubject(username)
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + 86400000)) // 1 día
-                .signWith(SignatureAlgorithm.HS256, jwtSecret.getEncoded())
+                .signWith(jwtSecret, SignatureAlgorithm.HS256)
                 .compact();
     }
 
     public String extractEmail(String token) {
-        return Jwts.parser()
+        return Jwts.parserBuilder()
                 .setSigningKey(jwtSecret.getEncoded())
+                .build()
                 .parseClaimsJws(token)
                 .getBody()
                 .getSubject();
     }
 
     private boolean isTokenExpired(String token) {
-        Date expiration = Jwts.parser()
+        Date expiration = Jwts.parserBuilder()
                 .setSigningKey(jwtSecret.getEncoded())
+                .build()
                 .parseClaimsJws(token)
                 .getBody()
                 .getExpiration();
