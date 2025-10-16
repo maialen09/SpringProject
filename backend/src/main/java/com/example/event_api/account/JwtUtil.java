6668796380fd1a8d16254702d5a9897cf1c0d04b
gcoearcher <gcoearchermaillo@gmail.com>
2025-10-16 08:38:06 +0000

    update AccountController

--- backend/src/main/java/com/example/event_api/account/JwtUtil.java
+++ backend/src/main/java/com/example/event_api/account/JwtUtil.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Component;
 import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
 import java.util.Date;
+import io.jsonwebtoken.SignatureAlgorithm;
 
 @Component
 public class JwtUtil {
--- backend/src/main/java/com/example/event_api/controller/AccountController.java
+++ backend/src/main/java/com/example/event_api/controller/AccountController.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/account")
 @CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
